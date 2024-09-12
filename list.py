8e018145927bb621725985a38161c938f714efff
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-09-12 09:23:26 -0500

    First commit

---
+++ list.py
@@ -0,0 +1,12 @@
+capitals = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford'
+}
+
+def list (dict):
+    pass
